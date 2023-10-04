9edc10ad3e38b76f93370e918b82fa8be4604b5f
kouwa stephane <kksasteph888@gmail.com>
2023-10-04 03:55:34 +0200

    maj

--- src/Controller/RatingAppController.php
+++ src/Controller/RatingAppController.php
@@ -8,18 +8,18 @@ use Drupal\Core\Controller\ControllerBase;
  * Returns responses for rating_app routes.
  */
 class RatingAppController extends ControllerBase {
-
+  
   /**
    * Builds the response.
    */
   public function build() {
-
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#markup' => $this->t('It works!')
     ];
-
+    $comment = $this->entityTypeManager()->getStorage('comment')->load(1);
+    dump($comment->toArray());
     return $build;
   }
-
+  
 }
